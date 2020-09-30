1d0289aba10c0ac2d44c6e2c1078275701814eae
Shan <coolmrsam3462521@gmail.com>
2020-09-30 10:46:58 +0800

    Implement isPowerofFour function

---
+++ isPowerOfFour.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* Given a signed number and to calculate whether the input number
+ * is the power of four or not
+ * I implement two versions of function, one is solving by loops
+ * and one is without loops
+ */
+bool isPowerOfFour_loop(int num)
+{
+    if (num <= 0) return false;
+    int r=0;
+    while (r == 0 && num > 1) {
+        r = num % 4;
+        num /= 4;
+    }
+    return r == 0;
+}
+bool isPowerOfFour(int num)
+{
+    return num > 0 && (num & (num - 1))==0 &&
+	   !(__builtin_ctz(num) & 0x1);
+}
+
+int main()
+{
+    printf("Input here...");
+    int num;
+    scanf("%d", &num);
+    if (isPowerOfFour(num)) {
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+    return 0;
+}
